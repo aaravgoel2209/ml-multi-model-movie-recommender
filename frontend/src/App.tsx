9729b82446c07e1a4a050365a4dca208e3884b86
Évence Batobo <126938639+Asvo777@users.noreply.github.com>
2026-02-17 18:31:29 -0500

    Initialize Frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <div>
+          <h1>Search Movies</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
